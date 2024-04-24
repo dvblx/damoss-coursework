5dd81052ac17ece3ed196457408131fc6daeb202
dvblx <artmel03@gmail.com>
2024-04-24 23:11:46 +0300

    exit button in header

--- src/main/java/ru/melnikov/computershop/controller/mvc/HomeController.java
+++ src/main/java/ru/melnikov/computershop/controller/mvc/HomeController.java
@@ -1,7 +1,9 @@
 package ru.melnikov.computershop.controller.mvc;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
@@ -10,5 +12,10 @@ public class HomeController {
     @GetMapping
     public String homeController(){
         return "redirect:/product";
+    }
+
+    @PostMapping
+    public void exitController(){
+        System.exit(130);
     }
 }
